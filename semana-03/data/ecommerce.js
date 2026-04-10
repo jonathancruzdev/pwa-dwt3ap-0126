bbe911fb445a6cc5113e741ca4e8fdda8593f6bc
jcruzemae <107081665+jcruzemae@users.noreply.github.com>
2026-04-10 00:05:02 -0300

    Semana 03 - Promesas

---
+++ semana-03/data/ecommerce.js
@@ -0,0 +1,52 @@
+export function getUsers() {
+  return new Promise((resolve) => {
+    console.log("⏳ Obteniendo usuarios...");
+
+    setTimeout(() => {
+      const users = [
+        { id: 1, name: "Juan" },
+        { id: 2, name: "Ana" }
+      ];
+
+      console.log("✅ Usuarios obtenidos");
+      resolve(users);
+    }, 1500);
+  });
+}
+
+
+export function getCartsByUser(userId) {
+  return new Promise((resolve) => {
+    console.log(`⏳ Obteniendo carrito del usuario ${userId}...`);
+
+    setTimeout(() => {
+      const carts = {
+        1: { userId: 1, products: [101, 102] },
+        2: { userId: 2, products: [103] }
+      };
+
+      console.log("✅ Carrito obtenido");
+      resolve(carts[userId]);
+    }, 1500);
+  });
+}
+
+
+export function getProductsByIds(productIds) {
+  return new Promise((resolve) => {
+    console.log("⏳ Obteniendo productos...");
+
+    setTimeout(() => {
+      const allProducts = {
+        101: { id: 101, name: "Notebook", price: 1000 },
+        102: { id: 102, name: "Mouse", price: 50 },
+        103: { id: 103, name: "Teclado", price: 80 }
+      };
+
+      const products = productIds.map(id => allProducts[id]);
+
+      console.log("✅ Productos obtenidos");
+      resolve(products);
+    }, 1500);
+  });
+}
---
+++ semana-03/script.js
@@ -0,0 +1,14 @@
+import { getUsers, getCartsByUser, getProductsByIds}  from './data/ecommerce.js'
+
+const btnIniciar = document.querySelector('#btn-iniciar');
+
+
+btnIniciar.addEventListener('click', () => {
+    console.log("Inicio");
+
+        
+
+    console.log("Fin");
+
+})
+
